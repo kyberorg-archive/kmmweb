d323ba526003875a0ed3269593108f270aecb17d
Alex Muravya <asm@virtalab.net>
2018-06-08 08:09:51 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# KMyMoney Web
+Yet another attempt to make kMyMoney web interface
+
+## Technologies used
+Spring Boot, Vaadin
+
---
+++ src/main/java/ee/yadev/kmmweb/KmmwebApplication.java
@@ -0,0 +1,12 @@
+package ee.yadev.kmmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmmwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmmwebApplication.class, args);
+	}
+}
